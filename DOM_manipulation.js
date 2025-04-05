43502f304dab81512a6e8851feb1ef4e1dfa8a63
pupuduPrasad <prasadjayasinghe142@gmail.com>
2025-04-05 15:13:59 +0530

    declaration functions

---
+++ DOM_manipulation.js
@@ -0,0 +1,13 @@
+alert("hellow")
+var h1_elements  = document.getElementsByTagName("h1")
+console.log(h1_elements[0])
+console.log(h1_elements[1])
+
+h1_elements[0].innerText="welcome to my youtube channel"
+console.log(h1_elements[0].innerHTML)
+
+var getSubjectId =document.getElementById("sub1")
+console.log(getSubjectId[0])
+
+
+
